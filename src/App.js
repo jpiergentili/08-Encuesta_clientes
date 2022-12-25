041590de7841e211932f91e4b021c9a7f878c1b8
jpiergentili <javypier1@gmail.com>
2022-12-25 12:46:51 -0300

    v6-funcionando con react components y todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Nav from "./components/Nav"
+
 function App() {
-    return  <div>
-                <h1>hola mundo desde parcel</h1>
-            </div>
+    return  <>
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about" element={<About />} />
+
+                    {/* Using path="*"" means "match anything", so this route
+                    acts like a catch-all for URLs that we don't have explicit
+                    routes for. */}
+                    <Route path="*" element={<Home />} />
+                </Routes>
+            </>
 }
 
 export default App
